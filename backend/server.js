30c319a1dc3a0e79d40c2285a571e0e967731bfd
TTBinh <tongtranbinh3008@gmail.com>
2026-05-27 20:27:04 +0700

    cide

--- backend/server.js
+++ backend/server.js
@@ -331,6 +331,8 @@ app.delete("/users/:id", async (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
